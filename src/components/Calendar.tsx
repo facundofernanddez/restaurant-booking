0bc995a65130391b7dbb9931396cb57098a3c414
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2024-01-07 18:10:13 -0300

    create calendar and constants

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,16 +1,74 @@
 "use client";
 
+import { useState } from "react";
 import ReactCalendar from "react-calendar";
+import { add, format } from "date-fns";
+import {
+  INTERVAL,
+  STORE_CLOSING_TIME,
+  STORE_OPENING_TIME,
+} from "@/constants/config";
+
+interface DateType {
+  justDate: Date | null;
+  dateTime: Date | null;
+}
 
 export default function Calendar() {
+  const [date, setDate] = useState<DateType>({
+    justDate: null,
+    dateTime: null,
+  });
+
+  console.log(date.dateTime);
+
+  const getTimes = () => {
+    if (!date.justDate) return;
+
+    const { justDate } = date;
+
+    const beginning = add(justDate, { hours: STORE_OPENING_TIME });
+    const end = add(justDate, { hours: STORE_CLOSING_TIME });
+    const interval = INTERVAL;
+
+    const times = [];
+    for (let i = beginning; i <= end; i = add(i, { minutes: interval })) {
+      times.push(i);
+    }
+
+    return times;
+  };
+
+  const times = getTimes();
+
   return (
-    <div>
-      <ReactCalendar
-        minDate={new Date()}
-        className="react-calendar"
-        view="month"
-        onClickDay={(date) => console.log(date)}
-      />
+    <div className="h-screen flex flex-col justify-center items-center">
+      {date.justDate ? (
+        <div className="flex gap-4">
+          {times?.map((time, index) => (
+            <div
+              key={`time-${index}`}
+              className="rounded-sm bg-gray-100 p-2 "
+            >
+              <button
+                type="button"
+                onClick={() => setDate((prev) => ({ ...prev, dateTime: time }))}
+              >
+                {format(time, "kk:mm")}
+              </button>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <ReactCalendar
+          minDate={new Date()}
+          className="react-calendar p-2"
+          view="month"
+          onClickDay={(date) =>
+            setDate((prev) => ({ ...prev, justDate: date }))
+          }
+        />
+      )}
     </div>
   );
 }
